56995850078a8566eb0e97266efb4ceca9f33d61
DESKTOP-CGLOUIV\91973 <ajoyr118@gmail.com>
2024-06-26 21:44:12 +0530

    removed comment

--- DevoPS/Controllers/HomeController.cs
+++ DevoPS/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 
 namespace DevoPS.Controllers
 {
-    //Add comment
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
